267a9ed82e767bdea3f00d2e67ee57eec87f85c4
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-04 14:29:11 -0700

    1.1

--- AdventOfCode2022.Day1/Program.cs
+++ AdventOfCode2022.Day1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var input1Stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AdventOfCode2022.Day1.Input1.txt")!;
+StreamReader reader = new(input1Stream);
+var current = 0;
+var max = 0;
+while (reader.ReadLine() is { } line)
+{
+    if (line == string.Empty)
+    {
+        if (current > max)
+            max = current;
+        current = 0;
+    }
+    else
+        current += int.Parse(line);
+}
+Console.WriteLine(max);
+

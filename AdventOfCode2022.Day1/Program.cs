e325d8a1dfe1b67bfe4a6b7806943e1c587ae30c
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-04 14:32:53 -0700

    1.2

--- AdventOfCode2022.Day1/Program.cs
+++ AdventOfCode2022.Day1/Program.cs
@@ -4,16 +4,24 @@ var input1Stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Ad
 StreamReader reader = new(input1Stream);
 var current = 0;
 var max = 0;
+var max2 = 0;
+var max3 = 0;
 while (reader.ReadLine() is { } line)
 {
     if (line == string.Empty)
     {
         if (current > max)
+        {
+            max3 = max2;
+            max2 = max;
             max = current;
+        }
+
         current = 0;
     }
     else
         current += int.Parse(line);
 }
 Console.WriteLine(max);
+Console.WriteLine(max + max2 + max3);
 

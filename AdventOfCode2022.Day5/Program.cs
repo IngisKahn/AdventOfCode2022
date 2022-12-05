0681d8d1ea9cfc9828b75ac3bb125b1ed51b99d1
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-04 21:48:43 -0700

    5.0

---
+++ AdventOfCode2022.Day5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+await using var inputStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AdventOfCode2022.Day5.Input.txt")!;
+using StreamReader reader = new(inputStream);
+var a = 0;
+var b = 0;
+
+while (await reader.ReadLineAsync() is { } line)
+{
+
+}
+Console.WriteLine(a);
+Console.WriteLine(b);

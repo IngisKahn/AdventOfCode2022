ca40565edde59bda95c0f04007844399d93cb2eb
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-04 20:52:48 -0700

    3.1

---
+++ AdventOfCode2022.Day3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+await using var inputStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AdventOfCode2022.Day3.Input.txt")!;
+using StreamReader reader = new(inputStream);
+var score = 0;
+int ItemValue(char item) => item switch {>= 'a' => item - 'a', _ => item - 'A' + 26};
+while (await reader.ReadLineAsync() is { } line)
+{
+    var flags = 0ul;
+    var chars = line.Length >> 1;
+    for (var i = 0; i < chars; i++)
+        flags |= 1ul << ItemValue(line[i]);
+    for (var i = 0; i < chars; i++)
+    {
+        var val = ItemValue(line[chars + i]);
+        if ((flags & 1ul << val) != 0)
+        {
+            score += val + 1;
+            break;
+        }
+    }
+}
+Console.WriteLine(score);

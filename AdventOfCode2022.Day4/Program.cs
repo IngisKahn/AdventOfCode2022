1baf6e6dcb5ba81bf4874f11d869d0bcd880c9ea
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-04 21:44:31 -0700

    4.2

---
+++ AdventOfCode2022.Day4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+await using var inputStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AdventOfCode2022.Day4.Input.txt")!;
+using StreamReader reader = new(inputStream);
+var coveredPairs = 0;
+var overlappingPairs = 0;
+
+while (await reader.ReadLineAsync() is { } line)
+{
+    var values = line.Split('-', ',').Select(int.Parse).ToArray();
+    if (values[0] <= values[2] && values[1] >= values[3] || values[0] >= values[2] && values[1] <= values[3])
+    {
+        coveredPairs++;
+        overlappingPairs++;
+    }
+    else if (values[1] >= values[2] && values[3] >= values[0])
+        overlappingPairs++;
+}
+Console.WriteLine(coveredPairs);
+Console.WriteLine(overlappingPairs);

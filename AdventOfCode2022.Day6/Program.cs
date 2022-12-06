79385a2c40b11146a22572092a07801499e6f826
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-05 22:26:58 -0700

    6.2

---
+++ AdventOfCode2022.Day6/Program.cs
@@ -0,0 +1,54 @@
+﻿using StreamReader reader = new("Input.txt");
+var data = reader.ReadToEnd();
+for (var i = 3; i < data.Length; i++)
+{
+    int? dupe = null;
+    for (var j = i - 3; j < i; j++)
+    {
+        for (var k = j + 1; k <= i; k++)
+            if (data[k] == data[j])
+            {
+                dupe = j;
+                break;
+            }
+
+        if (dupe != null)
+        {
+            break;
+        }
+    }
+
+    if (dupe == null)
+    {
+        Console.WriteLine(i + 1);
+        break;
+    }
+
+    i = dupe.Value + 3;
+}
+for (var i = 13; i < data.Length; i++)
+{
+    int? dupe = null;
+    for (var j = i - 13; j < i; j++)
+    {
+        for (var k = j + 1; k <= i; k++)
+            if (data[k] == data[j])
+            {
+                dupe = j;
+                break;
+            }
+
+        if (dupe != null)
+        {
+            break;
+        }
+    }
+
+    if (dupe == null)
+    {
+        Console.WriteLine(i + 1);
+        break;
+    }
+
+    i = dupe.Value + 13;
+}

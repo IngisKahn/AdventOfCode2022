8a586761ed8c147201f28a9cde993acd8efd0d0e
Jerry Toscano <Jerry.Toscano@strongmind.com>
2022-12-06 23:21:13 -0700

    7.2

--- AdventOfCode2022.Day6/Program.cs
+++ AdventOfCode2022.Day6/Program.cs
@@ -1,54 +1,32 @@
 ﻿using StreamReader reader = new("Input.txt");
-var data = reader.ReadToEnd();
-for (var i = 3; i < data.Length; i++)
-{
-    int? dupe = null;
-    for (var j = i - 3; j < i; j++)
-    {
-        for (var k = j + 1; k <= i; k++)
-            if (data[k] == data[j])
-            {
-                dupe = j;
-                break;
-            }
+string data = reader.ReadToEnd();
 
-        if (dupe != null)
+void FindUnique(int length)
+{
+    for (var i = length; i < data.Length; i++)
+    {
+        int? dupe = null;
+        for (var j = i - length; j < i; j++)
         {
+            for (var k = j + 1; k <= i; k++)
+                if (data[k] == data[j])
+                {
+                    dupe = j;
+                    break;
+                }
+
+            if (dupe != null)
+                break;
+        }
+
+        if (dupe == null)
+        {
+            Console.WriteLine(i + 1);
             break;
         }
-    }
 
-    if (dupe == null)
-    {
-        Console.WriteLine(i + 1);
-        break;
+        i = dupe.Value + length;
     }
-
-    i = dupe.Value + 3;
 }
-for (var i = 13; i < data.Length; i++)
-{
-    int? dupe = null;
-    for (var j = i - 13; j < i; j++)
-    {
-        for (var k = j + 1; k <= i; k++)
-            if (data[k] == data[j])
-            {
-                dupe = j;
-                break;
-            }
-
-        if (dupe != null)
-        {
-            break;
-        }
-    }
-
-    if (dupe == null)
-    {
-        Console.WriteLine(i + 1);
-        break;
-    }
-
-    i = dupe.Value + 13;
-}
+FindUnique(3);
+FindUnique(13);
---
+++ AdventOfCode2022.Day7/Program.cs
@@ -0,0 +1,53 @@
+﻿using StreamReader reader = new("Input.txt");
+
+Dir root = new(null);
+var current = root;
+await reader.ReadLineAsync();
+while (await reader.ReadLineAsync() is { } line)
+{
+    switch (line[0])
+    {
+        case '$':
+            if (line[2] == 'c')
+                current = line[5..] switch {"/" => root, ".." => current!.Parent, { } name => current!.AddDir(name)};
+            continue;
+        case 'd': break;
+        default:
+            current!.AddSize(int.Parse(line[..line.IndexOf(' ')]));
+            break;
+    }
+}
+
+Console.WriteLine(root.TotalSmallSize);
+Console.WriteLine(root.FindTargetSize(30000000 - (70000000 - root.Size)));
+
+class Dir
+{
+    public Dir(Dir? parent) => this.Parent = parent;
+    public Dir? Parent { get; }
+    public Dictionary<string, Dir> Dirs { get; } = new();
+
+    public Dir AddDir(string name)
+    {
+        Dir d = new(this);
+        this.Dirs[name] = d;
+        return d;
+    }
+
+    public long Size { get; private set; }
+
+    public void AddSize(int size)
+    {
+        this.Size += size;
+        this.Parent?.AddSize(size);
+    }
+
+    public long TotalSmallSize => (this.Size > 100000 ? 0 : this.Size) + this.Dirs.Values.Sum(d => d.TotalSmallSize);
+
+    public long FindTargetSize(long target, long best = long.MaxValue)
+    {
+        if (this.Size > target && this.Size < best)
+            best = this.Size;
+        return long.Min(best, this.Dirs.Count > 0 ? this.Dirs.Values.Min(d => d.FindTargetSize(target, best)) : best);
+    }
+}
